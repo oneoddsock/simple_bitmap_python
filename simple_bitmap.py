54dbe2501e56d6408d667a8f70b0869c6111e313
Brent Scriver <bscriver@oneoddsock.com>
2024-10-22 10:21:03 -0400

    Commenting, improving readability.

--- simple_bitmap.py
+++ simple_bitmap.py
@@ -1,5 +1,11 @@
 class SimpleBitmap:
-    """Simple bitmap class to generate BMP files."""
+    """
+    Simple class to generate BMP files.
+    Most of this was gleaned from https://en.wikipedia.org/wiki/BMP_file_format.
+    The BMP files generated are uncompressed.
+    Only 24bpp (no alpha channel) and 32bpp (with alpha channel) are supported.
+    The algorithm is extremely basic to minimize dependencies.
+    """
     
     width = 0
     height = 0
@@ -13,27 +19,27 @@ class SimpleBitmap:
         self.width = width
         self.height = height
         self.withAlpha = withAlpha
-        # The stride is the number of bytes in a row of pixels. It is padded to a multiple of 4 bytes.
-        # This is a little bit twiddling trick to achieve that, so that if it is already a multiple of
-        # 4, it will not change, otherwise it will be rounded up to the next multiple.
-        self.stride = (self.width * 3 + 3) & ~3
 
         dibSize = 40 # BITMAPINFOHEADER
         bitsPerPixel = 24
-        extraBytes = 0
 
         if self.withAlpha:
             dibSize = 56 # BITMAPV3INFOHEADER
-            self.stride = self.width * 4
-            bitsPerPixel = 32
-            extraBytes = 16
+            bitsPerPixel = 32 # include alpha channel
 
-        self.pixelOffset = 14 + dibSize + extraBytes
         self.bytesPerPixel = bitsPerPixel // 8
+
+        # The stride is the number of bytes in a row of pixels. It is padded to a multiple of 4 bytes.
+        # This is a little bit twiddling trick to achieve that, so that if it is already a multiple of
+        # 4, it will not change, otherwise it will be rounded up to the next multiple.
+        self.stride = (self.width * self.bytesPerPixel + 3) & ~3
+
+        self.pixelOffset = 14 + dibSize
 
         self.__init_bmp(dibSize, bitsPerPixel)
 
     def __fix_value(self, value):
+        """Ensures pixel values are integers in the range 0-255."""
         return max(0, min(255, int(value)))
     
     def set_pixel(self, x, y, r, g, b, a = 255):
@@ -61,11 +67,8 @@ class SimpleBitmap:
             a = self.bmp[offset + 3]
         return (r, g, b, a)
 
-    def __copy_bytes(self, dest, offset, src):
-        for i in range(len(src)):
-            dest[offset + i] = src[i]
-
     def __get_unique_color_count(self):
+        """Returns the number of unique colors in the image."""
         map = {}
         for y in range(self.height):
             for x in range(self.width):
@@ -77,32 +80,41 @@ class SimpleBitmap:
                     map[pixelValue] = 1
         return len(map)
     
-    def __init_bmp(self, dibSize, bitsPerPixel):
-        planes = 1
-        zero = 0
-        pixPerMeter = 2835
-        size = self.pixelOffset + self.height * self.stride
+    def __copy_literal(self, dest, offset, src):
+        """Applies the literal bytes in src to dest at the given offset."""
+        for i in range(len(src)):
+            dest[offset + i] = src[i]
 
-        self.bmp = bytearray(size)
+    def __copy_number(self, dest, offset, number, signed = False):
+        """Applies the number to dest at the given offset in little endian format, with the signed option."""
+        self.__copy_literal(dest, offset, number.to_bytes(4, 'little', signed=signed))
+
+    def __init_bmp(self, dibSize, bitsPerPixel):
+        """Initializes the BMP file with the given DIB size and bits per pixel."""
+        zero = [0, 0, 0, 0]
+        pixPerMeter = (2835).to_bytes(4, 'little') # 72 DPI
+        fileSize = self.pixelOffset + self.height * self.stride
+
+        self.bmp = bytearray(fileSize)
 
         # Bitmap file header 
-        self.__copy_bytes(self.bmp, 0, [0x42, 0x4D]) # BM
-        self.__copy_bytes(self.bmp, 2, size.to_bytes(4, 'little')) # File size
-        self.__copy_bytes(self.bmp, 6, [0, 0, 0, 0]) # Reserved
-        self.__copy_bytes(self.bmp, 10, self.pixelOffset.to_bytes(4, 'little'))
+        self.__copy_literal(self.bmp, 0, [0x42, 0x4D]) # BM
+        self.__copy_number(self.bmp, 2, fileSize) # File size
+        self.__copy_literal(self.bmp, 6, zero) # Reserved
+        self.__copy_number(self.bmp, 10, self.pixelOffset)
 
         # DIB header
-        self.__copy_bytes(self.bmp, 14, dibSize.to_bytes(4, 'little'))
-        self.__copy_bytes(self.bmp, 18, self.width.to_bytes(4, 'little'))
-        self.__copy_bytes(self.bmp, 22, (-self.height).to_bytes(4, 'little', signed=True)) # Negative height for top-down bitmap
-        self.__copy_bytes(self.bmp, 26, planes.to_bytes(2, 'little')) # Planes
-        self.__copy_bytes(self.bmp, 28, bitsPerPixel.to_bytes(2, 'little'))
-        self.__copy_bytes(self.bmp, 30, zero.to_bytes(4, 'little')) # Compression BI_RGB -- no compression
-        self.__copy_bytes(self.bmp, 34, zero.to_bytes(4, 'little')) # Image size -- can use 0 for BI_RGB
-        self.__copy_bytes(self.bmp, 38, pixPerMeter.to_bytes(4, 'little')) # X pixels per meter
-        self.__copy_bytes(self.bmp, 42, pixPerMeter.to_bytes(4, 'little')) # Y pixels per meter
-        self.__copy_bytes(self.bmp, 46, zero.to_bytes(4, 'little')) # Colors used
-        self.__copy_bytes(self.bmp, 50, zero.to_bytes(4, 'little')) # Important colors
+        self.__copy_number(self.bmp, 14, dibSize) # DIB header size
+        self.__copy_number(self.bmp, 18, self.width) # Image width
+        self.__copy_number(self.bmp, 22, -self.height, True) # Negative height for top-down bitmap
+        self.__copy_number(self.bmp, 26, 1) # Planes
+        self.__copy_number(self.bmp, 28, bitsPerPixel) # Bits per pixel
+        self.__copy_literal(self.bmp, 30, zero) # Compression BI_RGB -- no compression
+        self.__copy_literal(self.bmp, 34, zero) # Image size -- can use 0 for BI_RGB
+        self.__copy_literal(self.bmp, 38, pixPerMeter) # X pixels per meter
+        self.__copy_literal(self.bmp, 42, pixPerMeter) # Y pixels per meter
+        self.__copy_literal(self.bmp, 46, zero) # Colors used
+        self.__copy_literal(self.bmp, 50, zero) # Important colors
 
         # Alpha mask for V3 header
         if self.withAlpha:
@@ -113,14 +125,14 @@ class SimpleBitmap:
             rgbaMask = [0x0000FF00, 0x00FF0000, 0xFF000000, 0x000000FF] # RGBA order
             for i in range(4):
                 mask = rgbaMask[i].to_bytes(4, 'big') # We use big endian here to match the layout on disk.
-                self.__copy_bytes(self.bmp, 54 + i * 4, mask)
+                self.__copy_literal(self.bmp, 54 + i * 4, mask)
 
 
     def get_image(self):
         """Generates the image as a bitmap."""
 
         result = bytearray(self.bmp)
-        self.__copy_bytes(result, 46, self.__get_unique_color_count().to_bytes(4, 'little'))
+        self.__copy_literal(result, 46, self.__get_unique_color_count().to_bytes(4, 'little'))
         return result
 
     def __str__(self):
