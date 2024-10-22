529576fed1f6224ef6fd4104759b675d93ef6057
Brent Scriver <bscriver@oneoddsock.com>
2024-10-22 10:51:37 -0400

    Add more comments.

--- simple_bitmap.py
+++ simple_bitmap.py
@@ -48,6 +48,7 @@ class SimpleBitmap:
         assert y >= 0 and y < self.height
         offset = self.pixelOffset + y * self.stride + x * self.bytesPerPixel
         # The ordering seems odd, but it is because the pixel data is stored as ARGB in little endian, so it serializes as BGRA.
+        # When BGRA is loaded on a little endian platform, it appears as ARGB in memory.
         self.bmp[offset] = self.__fix_value(b)
         self.bmp[offset + 1] = self.__fix_value(g)
         self.bmp[offset + 2] = self.__fix_value(r)
@@ -91,6 +92,13 @@ class SimpleBitmap:
 
     def __init_bmp(self, dibSize, bitsPerPixel):
         """Initializes the BMP file with the given DIB size and bits per pixel."""
+        # The BMP file uses little endian format to represent numbers.
+        # Endian is how the byte order is represented on disk (there is also an equivalent for bit layout, too).
+        # For example, the value 0x12345678 would be represented as:
+        #  0x12, 0x34, 0x56, 0x78 - big endian -- the bytes are in the same order as the number (also known as the most significant byte first)
+        #  0x78, 0x56, 0x34, 0x12 - little endian -- the bytes are reversed (also known as the least significant byte first)
+        # The reason why BMP files do this is because they were designed on the original intel processors which were little endian.
+        # It allows bitmaps to be loaded directly into memory and used without any conversion on those processors.
         zero = [0, 0, 0, 0]
         pixPerMeter = (2835).to_bytes(4, 'little') # 72 DPI
         fileSize = self.pixelOffset + self.height * self.stride
@@ -101,20 +109,20 @@ class SimpleBitmap:
         self.__copy_literal(self.bmp, 0, [0x42, 0x4D]) # BM
         self.__copy_number(self.bmp, 2, fileSize) # File size
         self.__copy_literal(self.bmp, 6, zero) # Reserved
-        self.__copy_number(self.bmp, 10, self.pixelOffset)
+        self.__copy_number(self.bmp, 10, self.pixelOffset) # The offset to the pixel data in the file.
 
         # DIB header
         self.__copy_number(self.bmp, 14, dibSize) # DIB header size
         self.__copy_number(self.bmp, 18, self.width) # Image width
         self.__copy_number(self.bmp, 22, -self.height, True) # Negative height for top-down bitmap
-        self.__copy_number(self.bmp, 26, 1) # Planes
-        self.__copy_number(self.bmp, 28, bitsPerPixel) # Bits per pixel
+        self.__copy_number(self.bmp, 26, 1) # Planes (always 1)
+        self.__copy_number(self.bmp, 28, bitsPerPixel) # Bits per pixel (24 or 32 in our case)
         self.__copy_literal(self.bmp, 30, zero) # Compression BI_RGB -- no compression
         self.__copy_literal(self.bmp, 34, zero) # Image size -- can use 0 for BI_RGB
-        self.__copy_literal(self.bmp, 38, pixPerMeter) # X pixels per meter
-        self.__copy_literal(self.bmp, 42, pixPerMeter) # Y pixels per meter
-        self.__copy_literal(self.bmp, 46, zero) # Colors used
-        self.__copy_literal(self.bmp, 50, zero) # Important colors
+        self.__copy_literal(self.bmp, 38, pixPerMeter) # X pixels per meter: 72 DPI
+        self.__copy_literal(self.bmp, 42, pixPerMeter) # Y pixels per meter: 72 DPI
+        self.__copy_literal(self.bmp, 46, zero) # Colors used -- we update this later when we get an image
+        self.__copy_literal(self.bmp, 50, zero) # Important colors -- zero implies all of them are important
 
         # Alpha mask for V3 header
         if self.withAlpha:
@@ -142,7 +150,7 @@ class SimpleBitmap:
         """Generates the image as a bitmap."""
 
         result = bytearray(self.bmp)
-        self.__copy_literal(result, 46, self.__get_unique_color_count().to_bytes(4, 'little'))
+        self.__copy_number(result, 46, self.__get_unique_color_count())
         return result
 
     def __str__(self):
